0d18e259b85b22f9e16977a6c633ded7ceee31bf
Nafisa Axmed <nafisa@Nafisas-MacBook-Air.local>
2021-04-02 20:27:59 +0100

    solution for most char occurrences in string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,25 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
-
+function maxChar(str) {
+    let chars = {}
+    let maxNum = -Infinity
+    let maxChar=""
+    
+    for (let char of str){
+      if (!chars[char]){
+        chars[char] = 1
+      } else{
+        chars[char]++
+      }
+    }
+  
+    for (let property in chars){
+     if (chars[property] > maxNum){
+       maxNum = chars[property]
+       maxChar=property
+     }
+    }
+return maxChar
+}
 module.exports = maxChar;
